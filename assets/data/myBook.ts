b316aac8d23b85995884f207f358652018d3827d
koharshi <46941627+koharshi@users.noreply.github.com>
2020-07-31 22:41:35 -0400

    Add files via upload

---
+++ assets/data/myBook.ts
@@ -0,0 +1,8 @@
+import { bookInfo } from  '../../app/bookInfo';
+
+export const MYBOOKS: bookInfo[] = [
+{ bname: 'THE SCIENCE OF INTERSTELLAR', bauthor: 'Kip Thorne', bgenre: 'Sci-fi/Adventure', byear: 2014, bpicture: 'assets/images/Interstellar.jpg'},
+{ bname: 'THE MARTIAN', bauthor: 'Andy Weir', bgenre: 'Sci-fi/Adventure', byear: 2014, bpicture: 'assets/images/TheMartian.jpg'},
+{ bname: 'THE FOUNTAINHEAD', bauthor: 'Ayn Rand', bgenre: 'Psychological Fiction', byear: 1943, bpicture: 'assets/images/TheFountainHead.jpg'},
+{ bname: 'PEARL HARBOR', bauthor: 'Craig Nelson', bgenre: 'War', byear: 2016, bpicture: 'assets/images/PearlHarbor.jpg'},
+];
